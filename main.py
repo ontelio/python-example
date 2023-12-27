8db45ab5939bd12c13c70dce6fadecd702d8a6c1
Tommy Shrove <t.shrove@ontelio.com>
2023-12-27 07:28:54 -0600

    Update main.py
    Update main to add the new endpoint and fix a bug.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
 import time
 
-API_KEY = "API_KEY"
-API_URL = "https://yp1ypp2boj.execute-api.us-east-2.amazonaws.com/prod"
+#API_KEY = "API_KEY"
+API_KEY = "QWT6MNRtbK1hDEySneLHGuHMr44M4vP91q1EXh7a"
+API_URL = "https://api.censori.dev.ontelio.ai"
 
-# Send media to begin redaction process
-def send_media():
+# Send media to begin transcription and redaction process
+def transcribe_and_redact():
     """ Sends media to Ontelio's Redaction Engine. Input parameters in the form of json body.
     See code comments for explanation of each json key.
 
@@ -13,15 +14,13 @@ def send_media():
         e: Generic exception during key retrieval or media upload process.
     """
 
-    upload_key_endpoint = f"{API_URL}/redact/media"
+    upload_key_endpoint = f"{API_URL}/transcribe"
     # Call to get upload key
     upload_key_json_body = {
         "filename": "preamble.wav",  # Name of file to be redacted
         "callbackUrl": "http://test.io/",  # Callback URL to receive redacted transcript
-        "language": "en-US",  # Language that is being redacted in the transcript
-        "transcriptType": "VOCI",  # Type of transcript
-        "enableTranscriptRedaction": True,  # Toggle transcript redaction
         "enableMediaRedaction": True,  # Toggle media file redaction
+        #"entities": [] # you can leave the entities blank if you want to redact everything.
         "entities": ["PERSON", "LOCATION"]  # Types of entities to be redacted
     }
     # Header object
@@ -49,6 +48,8 @@ def send_media():
         except Exception as e:
             raise e
 
+        # you can use the polling endpoint for status updates or use the callback
+        # as defined in the post request above.
         poll_status(job_id=job_id)
 
 
@@ -69,10 +70,10 @@ def poll_status(job_id = None):
         print('Checking the status of the job.')
         response = requests.get(status_endpoint, headers=headers)
         response_json = response.json()
-        if response_json and response_json["status"] == "ERROR":
+        if response_json and 'status' in response_json and response_json["status"] == "ERROR":
             print("Error in redaction process")
             return
-        if response_json and response_json["status"] != "COMPLETED":
+        if response_json and 'status' in response_json and response_json["status"] != "COMPLETED":
             print(f'The current status of the job is {response_json["status"]}')
             time.sleep(1.0 - ((time.time() - start) % 1.0))
             poll_status(job_id=job_id)
@@ -83,5 +84,5 @@ def poll_status(job_id = None):
 
 
 if __name__ == '__main__':
-    send_media()
+    transcribe_and_redact()
 
